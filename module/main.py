87c7bf7dd30acfcc33b4dde88cfc1440c0373730
Shench35 <akinpelushuaib0@gmail.com>
2026-05-03 18:14:18 +0100

    fuck off joor

---
+++ module/main.py
@@ -0,0 +1,9 @@
+# main.py
+
+from input_module import get_score
+from logic_module import get_grade
+from output_module import display_result
+
+score = get_score()
+grade = get_grade(score)
+display_result(score, grade)
